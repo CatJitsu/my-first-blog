85e0c8f6095dae3efc8fcb48a6596df80f419c9c
Chris <cowrulez@gmail.com>
2026-03-28 11:56:53 -0400

    making website live

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_=@=4#dfgakqhgs-dg8=wb(az@^g(smq22q%tffb*x8yq7dz%u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
