5cf14a5c92c45e440d369ce3a438625029276770
Chris <cowrulez@gmail.com>
2026-03-28 11:47:37 -0400

    making website live

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_=@=4#dfgakqhgs-dg8=wb(az@^g(smq22q%tffb*x8yq7dz%u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
